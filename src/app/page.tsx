c4a16cb4b06852dc34e9a3c15eec85c57e5d7062
thesupersupersigma <thesupersupersigma@gmail.com>
2025-02-23 13:28:58 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>🚧 Under Construction 🚧</h1>
+      <p>ChatGPT Working Overtime For This</p>
+    </div>
+  );
+};
+
+export default Home;
